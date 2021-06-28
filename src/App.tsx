04bc6d5676f36e9fcc2784287047415641f63cab
Palpatio <vesta661@gmail.com>
2021-06-28 09:18:15 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className={'app-wrapper'}>
+            <div>
+                <ul>
+                    <li>Hello</li>
+                    <li>News</li>
+                    <li>Yo</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default App;
